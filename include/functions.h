4912bbf85c3f0706b898f82e18ece2a2071a6c87
Matheus de Andrade Silva <matheus.mas132@hotmail.com>
2019-04-19 12:36:05 -0300

    declaring functions

---
+++ include/functions.h
@@ -0,0 +1,37 @@
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+using Compare = bool (*)(const void *, const void *);
+using Predicate = bool (*)(const void *);
+using Equal = bool (*)(const void *, const void *);
+
+namespace graal{
+
+	const void * min( const void * first, const void * last, size_t size, Compare cmp );
+
+	void reverse( void * first, void * last, size_t size );
+
+	void * copy( const void * first, const void * last, const void * d_first, size_t size );
+
+	void * clone( const void * first, const void * last, size_t size );
+
+	const void * find_if( const void * first, const void * last, size_t size, Predicate p );
+
+	const void * find( const void * first, const void * last, size_t size, const void * value, Equal eq );
+
+	bool all_of( const void * first, const void * last, size_t size, Predicate p );
+	bool any_of( const void * first, const void * last, size_t size, Predicate p );
+	bool none_of( const void * first, const void * last, size_t size, Predicate p );
+
+	bool equal( const void * first1, const void * last1, const void * first2, Equal eq );
+	bool equal( const void * first1, const void * last1, const void * first2, const void * last2, Equal eq );
+
+	void * unique( void * first, void * last, size_t size, Equal eq );
+
+	void * partition( void * first, void * last, size_t size, Predicate p);
+
+	void * sort( void * first, size_t count, size_t size, Compare cmp );
+
+}
+
+#endif
---
+++ src/functions.cpp
@@ -0,0 +1,75 @@
+#include "functions.h"
+
+namespace graal{
+
+	const void * min( const void * first, const void * last, size_t size, Compare cmp )
+	{
+		// TODO: implement the function
+	}
+
+	void reverse( void * first, void * last, size_t size )
+	{
+		// TODO: implement the function
+	}
+
+	void * copy( const void * first, const void * last, const void * d_first, size_t size )
+	{
+		// TODO: implement the function
+	}
+
+	void * clone( const void * first, const void * last, size_t size )
+	{
+		// TODO: implement the function
+	}
+
+	const void * find_if( const void * first, const void * last, size_t size, Predicate p )
+	{
+		// TODO: implement the function
+	}
+
+	const void * find( const void * first, const void * last, size_t size, const void * value, Equal eq )
+	{
+		// TODO: implement the function
+	}
+
+	bool all_of( const void * first, const void * last, size_t size, Predicate p )
+	{
+		// TODO: implement the function
+	}
+
+	bool any_of( const void * first, const void * last, size_t size, Predicate p )
+	{
+		// TODO: implement the function
+	}
+
+	bool none_of( const void * first, const void * last, size_t size, Predicate p )
+	{
+		// TODO: implement the function
+	}
+
+	bool equal( const void * first1, const void * last1, const void * first2, Equal eq )
+	{
+		// TODO: implement the function
+	}
+
+	bool equal( const void * first1, const void * last1, const void * first2, const void * last2, Equal eq )
+	{
+		// TODO: implement the function
+	}
+
+	void * unique( void * first, void * last, size_t size, Equal eq )
+	{
+		// TODO: implement the function
+	}
+
+	void * partition( void * first, void * last, size_t size, Predicate p)
+	{
+		// TODO: implement the function
+	}
+
+	void * sort( void * first, size_t count, size_t size, Compare cmp )
+	{
+		// TODO: implement the function
+	}
+
+}
